7e46335b79d87bb2241c01eddcc120c4fe61c6fa
bored <brunoseguer@hotmail.com>
2018-03-11 19:25:08 -0300

    navbar added, still in progress

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import ApiClient from './ApiClient';
 import Converter from './containers/Converter.jsx';
-import conf from './config/default'
+import NavBar from './components/NavBar.jsx';
+import conf from './config/default';
 
 const client = new ApiClient(conf);
 
 const App = () => (
   <div>
+    <NavBar />
     <Converter client={client} />
   </div>
 );
